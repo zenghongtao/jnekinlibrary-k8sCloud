c632527e398da7c59c96377873209411fcd0de87
曾洪涛 <hongtao.zeng@talebase.com>
2020-12-25 15:16:19 +0800

    demo

--- vars/jiraDeploy.groovy
+++ vars/jiraDeploy.groovy
@@ -124,7 +124,8 @@ void call() {
                         // }
                         
                         // println(issues)
-                        println("HELLO")
+                        response = jira.RunJql(project%20%3D%20${projectKey}%20AND%20fixVersion%20%3D%20${versionName}%20AND%20issuetype%20%3D%20Task)
+                        println(response)
                         
                         //搜索gitlab分支是否已合并然后删除
                         
