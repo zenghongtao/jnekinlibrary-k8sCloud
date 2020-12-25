18614d0ee59777e5ee0ad9f91ca0a5aa0e28a3ce
曾洪涛 <hongtao.zeng@talebase.com>
2020-12-25 14:02:20 +0800

    demo

--- src/com/talebase/jira.groovy
+++ src/com/talebase/jira.groovy
@@ -3,16 +3,18 @@ package com.talebase
 //封装HTTP请求
 def HttpReq(reqType,reqUrl,reqBody){
     def apiServer = "http://10.99.76.225:30085/rest/api/2"
-   
-   result = httpRequest authentication: 'jira-admin-user',
-            httpMode: reqType, 
-            contentType: "APPLICATION_JSON",
-            consoleLogResponseBody: true,
-            ignoreSslErrors: true, 
-            requestBody: reqBody,
-            url: "${apiServer}/${reqUrl}",
-            validResponseCodes: '200:404'
-            // quiet: true
+
+    withCredentials([string(credentialsId: '1af6c2d7-8a28-4a24-a011-f646c643eeac', variable: 'jira-admin-user')]) {   
+        result = httpRequest authentication: "${jira-admin-user}",
+                httpMode: reqType, 
+                contentType: "APPLICATION_JSON",
+                consoleLogResponseBody: true,
+                ignoreSslErrors: true, 
+                requestBody: reqBody,
+                url: "${apiServer}/${reqUrl}",
+                validResponseCodes: '200:404'
+                // quiet: true
+    }
     return result
 }
 
