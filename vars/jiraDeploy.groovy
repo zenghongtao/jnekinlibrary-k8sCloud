98b47d53f5ec84c73fa4d62077e3f9c92e9b2138
曾洪涛 <hongtao.zeng@talebase.com>
2020-12-25 14:41:57 +0800

    demo

--- vars/jiraDeploy.groovy
+++ vars/jiraDeploy.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
-import com.talebase.jira.RunJql
+
 
 void call() {
     def gitlab = new com.talebase.gitlab()
-    // def jira = new com.talebase.jira()
+    def jira = new com.talebase.jira()
     def k8s = new com.talebase.kubernetes()
 
     pipeline {
@@ -106,7 +106,7 @@ void call() {
                     script{
                         //获取issuesName
                         println("project%20%3D%20${projectKey}%20AND%20fixVersion%20%3D%20${versionName}%20AND%20issuetype%20%3D%20Task")
-                        response = new RunJql("project%20%3D%20${projectKey}%20AND%20fixVersion%20%3D%20${versionName}%20AND%20issuetype%20%3D%20Task")
+                        response = jira.RunJql("project%20%3D%20${projectKey}%20AND%20fixVersion%20%3D%20${versionName}%20AND%20issuetype%20%3D%20Task")
                         
                         response = readJSON text: """${response.content}"""
                         println(response)
