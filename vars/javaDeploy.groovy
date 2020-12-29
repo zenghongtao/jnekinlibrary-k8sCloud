dfc73e453de44e233e763f45062550c4997c5d82
曾洪涛 <hongtao.zeng@talebase.com>
2020-12-29 17:33:58 +0800

    demo

--- vars/javaDeploy.groovy
+++ vars/javaDeploy.groovy
@@ -7,28 +7,7 @@ void call() {
     Object build = new build()
 
     pipeline{
-        agent {
-            kubernetes {
-                cloud "kubernetes"
-                yaml """
-    apiVersion: v1
-    kind: Pod
-    metadata:
-      namespace: devops
-    spec:
-      imagePullSecrets:
-        - name: aliyun-image
-      containers:
-        # maven
-        - name: jnlp-agent-maven
-          image: registry.cn-hangzhou.aliyuncs.com/zenghongtao/jnlp-agent-maven:3.6.3
-          imagePullPolicy: IfNotPresent
-          command:
-            - cat
-          tty: true
-    """
-            }
-        }
+        agent any
 
     triggers {
         GenericTrigger(
