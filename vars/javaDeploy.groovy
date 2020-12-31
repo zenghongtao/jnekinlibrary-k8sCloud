9e03eb4391fdbb211e4dc420fadd334897d076b5
root <admin@talebase.com>
2020-12-31 11:59:46 +0800

    test

--- vars/javaDeploy.groovy
+++ vars/javaDeploy.groovy
@@ -7,7 +7,31 @@ void call() {
     Object build = new build()
 
     pipeline{
-        agent any
+            cloud "kubernetes"
+            yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  namespace: devops
+spec:
+  imagePullSecrets:
+    - name: aliyun-image
+  containers:
+    # maven
+    - name: jnlp-agent-maven
+      image: registry.cn-hangzhou.aliyuncs.com/zenghongtao/jnlp-agent-maven:3.6.3
+      imagePullPolicy: IfNotPresent
+      command:
+        - cat
+      tty: true
+    volumeMounts:
+      - name: m2
+        mountPath: /root/.m2
+  volumes:
+    - name: m2
+      persistenVolumeClaim:
+        claimName: m2-pvc
+"""
 
     triggers {
         GenericTrigger(
